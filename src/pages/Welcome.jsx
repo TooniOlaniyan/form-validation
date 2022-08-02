c39cb3084093b785aa340f12f48d508489373513
TooniOlaniyan <toonilaniyan@gmail.com>
2022-08-02 12:55:54 +0100

    welcome

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+import TypeWriter from '../components/TypeWriter'
 
 function Welcome() {
   return (
-    <div>Welcome</div>
+    <Main>
+      <TypeWriter text={'WELCOME'}/>
+    </Main>
   )
 }
+
+const Main = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+  
+`
 
 export default Welcome
